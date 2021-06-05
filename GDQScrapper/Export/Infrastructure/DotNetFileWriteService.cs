eeeca4fdfc55d7b258aa1e32cce084c40f7f7472
DaRo <dario.ciarlantini@outlook.com>
2021-06-04 23:30:04 -0300

    Add DotNetFileIo DLL

--- GDQScrapper/Export/Infrastructure/DotNetFileWriteService.cs
+++ GDQScrapper/Export/Infrastructure/DotNetFileWriteService.cs
@@ -1,34 +1,19 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using GDQScrapper.Calendar.Domain;
+﻿using GDQScrapper.Calendar.Domain;
+using DotNetFileIO;
 
 namespace GDQScrapper.Export.Infrastructure
 {
     public class DotNetFileWriteService : IFileWriteService
     {
+        private FileService FileService = new FileService();
+
+        private readonly string path = "";
+
         public void ExportToFile(string file, string fileName, string extencion)
         {
-            fileName = fileName.Replace(" ", "_") + "." + extencion.ToLower();
+            extencion = extencion.ToLower();
 
-            try
-            {  
-                if (File.Exists(fileName))
-                {
-                    File.Delete(fileName);
-                    Console.WriteLine("Old file was removed");
-                }
-
-                using (FileStream fs = File.Create(fileName))
-                {
-                    byte[] author = new UTF8Encoding(true).GetBytes(file);
-                    fs.Write(author, 0, author.Length);
-                }
-            }
-            catch
-            {
-                throw;
-            }
+            FileService.SaveFile(file, path, fileName, extencion);
         }
     }
 }
